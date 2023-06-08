ff6a1cfa9e12efa0ccf7acebd82120aa0dd5ef08
Reric Young <Reric@outlook.com>
2023-06-08 19:16:31 +0800

    Change hyperparameters

--- pl_models.py
+++ pl_models.py
@@ -10,10 +10,12 @@ from models.loss import *
 
 
 class LDA_PL(pl.LightningModule):
-    def __init__(self, epoch, hyp):
+    def __init__(self, epoch, batch_size, hyp):
         super().__init__()
         self.save_hyperparameters()
         self.hyp = hyp
+        self.n_prototypes = self.hyp['n_prototypes']
+        self.batch_size = batch_size
         self.backbone = LDAModel(n_prototypes=self.hyp['n_prototypes'], n_features=self.hyp['n_features'])
         self.epoch = epoch
         self.cls_loss = nn.CrossEntropyLoss()
@@ -30,8 +32,8 @@ class LDA_PL(pl.LightningModule):
         loss = self.cls_loss(y_hat, y)
         pos, neg = self.backbone.read_prototype()
         loss += self.inter_loss(pos, neg) * self.hyp['inter_weight']
-        loss += self.intra_loss(pos, neg) * self.hyp['intra_weight']
-        loss += self.data_loss(dist, y) * self.hyp['data_weight']
+        loss += self.intra_loss(pos, neg) * self.hyp['intra_weight'] / (self.n_prototypes * (self.n_prototypes + 1) / 2)
+        loss += self.data_loss(dist, y) * self.hyp['data_weight'] / self.batch_size
         return loss
 
     def training_step(self, batch, batch_idx):
@@ -49,21 +51,15 @@ class LDA_PL(pl.LightningModule):
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        y_hat, dist = self.backbone(x)
-        loss = self.cal_loss(y_hat, dist, y)
+        y_hat, _ = self.backbone(x)
+        loss = self.cls_loss(y_hat, y)
         self.log('test_loss', loss, sync_dist=True)
 
     def configure_optimizers(self):
-        opt = torch.optim.SGD(self.parameters(), lr=self.hyp['lr'], momentum=self.hyp['momentum'],
-                              weight_decay=self.hyp['weight_decay'])
-        epoch_steps = [int(self.epoch * 0.1), int(self.epoch * 0.2), int(self.epoch * 0.3), int(self.epoch * 0.4)]
-        print('epoch_steps:', epoch_steps)
-
-        def lr_step_func(epoch):
-            return 0.1 ** len([m for m in epoch_steps if m <= epoch])
-
-        scheduler = torch.optim.lr_scheduler.LambdaLR(
-            optimizer=opt, lr_lambda=lr_step_func)
+        opt = torch.optim.SGD(self.parameters(), lr=self.hyp['lr'],
+                              momentum=self.hyp['momentum'], weight_decay=self.hyp['weight_decay'])
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            optimizer=opt, gamma=self.hyp['lr_decay'])
         lr_scheduler = {
             'scheduler': scheduler,
             'name': 'learning_rate',
